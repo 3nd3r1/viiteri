3a3e091441d6f81a4327af04c1a2b178110c561e
Jouko Mälkönen <jouko.malkonen@helsinki.fi>
2023-11-21 20:25:57 +0200

    article-lähdeviitteen lisäyksestä huolehtiva route luotu
    Co-authored-by: oskarruo <oskar.ruokolainen@gmail.com>

--- viiteri/app.py
+++ viiteri/app.py
@@ -1,10 +1,11 @@
 """ viiteri/app.py """
 from flask import Flask
 
-from viiteri.routes import index
+from viiteri.routes import index, create_reference
 
 app = Flask(__name__)
 app.register_blueprint(index.blueprint)
+app.register_blueprint(create_reference.blueprint)
 
 
 @app.route("/ping")
---
+++ viiteri/routes/create_reference.py
@@ -0,0 +1,25 @@
+""" viiteri/routes/create_reference.py """
+from flask import Blueprint, render_template, redirect, request, flash
+from services.reference_service import reference_service
+
+
+blueprint = Blueprint("create_reference", __name__)
+
+
+@blueprint.route("/create", methods=["GET", "POST"])
+def render_create():
+    """ Render create page """
+    if request.method == "GET":
+        return render_template("create.html")
+    if request.method == "POST":
+        author = request.form["author"]
+        title = request.form["title"]
+        journal = request.form["journal"]
+        year = request.form["year"]
+        volume = request.form["volume"]
+        try:
+            reference_service.create_reference(author, title, journal, year, volume)
+            flash("Reference created successfully!")
+        except Exception as error:
+            flash(str(error))
+        return redirect("/create")
