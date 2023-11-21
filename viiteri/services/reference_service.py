7a216bee781d8a66d6033a258adb4ed139a2f4f9
Jouko Mälkönen <jouko.malkonen@helsinki.fi>
2023-11-21 19:51:34 +0200

    pieni virhekorjaus
    Co-authored-by: oskarruo <oskar.ruokolainen@gmail.com>

--- viiteri/services/reference_service.py
+++ viiteri/services/reference_service.py
@@ -2,9 +2,12 @@ from repositories.reference_repository import ReferenceRepository as default_ref
 
 class ReferenceService:
     def __init__(self, reference_repository=default_reference_repository):
-        self._reference_repository = default_reference_repository
+        self._reference_repository = reference_repository
 
     def get_all_references(self):
         return self._reference_repository.get_all_references()
+    
+    def create_reference(self, author, title, journal, year, volume):
+        return self._reference_repository.create_reference(author, title, journal, year, volume)
     
 reference_service = ReferenceService()
