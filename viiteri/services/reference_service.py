62959cf7570e8ed3090efc1d4b63a706d6aecfc2
Jouko Mälkönen <jouko.malkonen@helsinki.fi>
2023-11-21 20:13:54 +0200

    pieni virheenkorjaus ja kommentoidaan tiedoston polku

--- viiteri/services/reference_service.py
+++ viiteri/services/reference_service.py
@@ -1,8 +1,9 @@
+""" viiteri/routes/reference_service """
 from repositories.reference_repository import ReferenceRepository as default_reference_repository
 
 class ReferenceService:
     def __init__(self, reference_repository=default_reference_repository):
-        self._reference_repository = default_reference_repository
+        self._reference_repository = reference_repository
 
     def get_all_references(self):
         return self._reference_repository.get_all_references()
