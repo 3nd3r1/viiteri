14be744b19dc64a573fb146ebb0a07bcab2c9683
isabel <isabel.suorsa@helsinki.fi>
2023-11-22 17:10:44 +0200

    reference repo unittest

--- viiteri/repositories/reference_repository.py
+++ viiteri/repositories/reference_repository.py
@@ -1,5 +1,6 @@
 """ viiteri/repositories/reference_repository.py """
 # from entities.article import Article
+from sqlalchemy.sql import text
 from viiteri.utils.db import get_database_connection
 
 
@@ -12,20 +13,26 @@ class ReferenceRepository:
     def add_reference(self, content):
         """Adds a new reference to the database."""
         cursor = self._connection.session()
-        cursor.execute(
-            'insert into reference_table (content) values (?);',
-            (content)
+        cursor.execute(text(
+            'insert into reference_table (content) values (:content);'),
+            {"content":content}
         )
-        self._connection.commit()
+        cursor.commit()
 
     def get_all_references(self):
         """Gets all references from the database"""
         cursor = self._connection.session()
-        cursor.execute(
-            'select * from reference_table;'
-        )
-        references = cursor.fetchall()
+        result = cursor.execute(text(
+            'select content from reference_table;'
+        ))
+        references = result.fetchall()
         return references
+    
+    def delete_all(self):
+        """Empties whole database"""
+        cursor = self._connection.session()
+        cursor.execute(text('delete from reference_table'))
+        cursor.commit()
 
 
 reference_repository = ReferenceRepository(get_database_connection())
