449c935b9c728bd87e5f35df6399eafcc6ddd9db
3nd3r1 <ender.spam.guru@gmail.com>
2023-11-22 18:46:03 +0200

    Fixataan pylint virhe

--- viiteri/services/reference_service.py
+++ viiteri/services/reference_service.py
@@ -1,8 +1,9 @@
 """ viiteri/services/reference_service """
 
+from pybtex.database import BibliographyData, Entry
+
 # pylint: disable-next=import-error, no-name-in-module
 from viiteri.repositories.article_repository import ReferenceRepository as default_repository
-from pybtex.database import BibliographyData, Entry
 
 
 class ReferenceService:
