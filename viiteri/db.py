e6b328054458aba79db3fda39b86a8ae90acea81
isabel <isabel.suorsa@helsinki.fi>
2023-11-22 12:13:01 +0200

    references-taulu uudelleennimetty

--- viiteri/db.py
+++ viiteri/db.py
@@ -1,6 +1,7 @@
-from app import app
+from viiteri.app import app
 from flask_sqlalchemy import SQLAlchemy
 import os
+
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 db = SQLAlchemy(app)
--- viiteri/initialize_db.py
+++ viiteri/initialize_db.py
@@ -1,17 +1,17 @@
-from db import get_database_connection
+from viiteri.db import get_database_connection
 
 
 def drop_table(connection):
     cursor = connection.cursor()
     cursor.execute("""
-        DROP TABLE IF EXISTS articles;
+        DROP TABLE IF EXISTS references;
     """)
     connection.commit()
 
 def create_table(connection):
     cursor = connection.cursor()
     cursor.execute("""
-        CREATE TABLE articles (
+        CREATE TABLE references (
             id SERIAL PRIMARY KEY,
             content TEXT
         );
