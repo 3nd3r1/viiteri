12b9cc143a9d4ca30fa1fcb5fea330ee10483811
3nd3r1 <ender.spam.guru@gmail.com>
2023-11-23 16:26:35 +0200

    Päivitetään entityä

--- viiteri/entities/article.py
+++
@@ -1,9 +0,0 @@
-"""viiteri/entities/article.py"""
-class Article:
-    """Class for article-type references"""
-    def __init__(self, author, title, journal, year, volume):
-        self.author = author
-        self.title = title
-        self.journal = journal
-        self.year = year
-        self.volume = volume
