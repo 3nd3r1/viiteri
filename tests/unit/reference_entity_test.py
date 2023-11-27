ec19e012a627e0c821c9039ab88c094935899c36
oskarruo <oskar.ruokolainen@gmail.com>
2023-11-27 15:16:16 +0200

    lisätty book-entityn testejä
    Co-authored-by: Jouko Mälkönen <jouko.malkonen@helsinki.fi>

--- tests/unit/reference_entity_test.py
+++ tests/unit/reference_entity_test.py
@@ -13,7 +13,7 @@ class TestReferenceEntity(unittest.TestCase):
                                     title="Petterin Kirja",
                                     journal="Petterin Kirjakokoelma", year="2003", volume="1")
         self.test_book = Book(cite_key="petkir", author="Petteri", 
-                              editor="Petteri", title="Petterin kirja vol 2", 
+                              editor="Petteri", title="Petterin Kirja vol 2", 
                               publisher="WSOY", year="2004")
 
     def test_article_constructor(self):
@@ -32,7 +32,7 @@ class TestReferenceEntity(unittest.TestCase):
         self.assertEqual(self.test_article.volume, "1")
     
     def test_book_constructor(self):
-        """ Test that Book-entity is correctly instantiated"""
+        """ Test that Book-entity is correctly instantiated """
         # Test that abstract fields are set
         self.assertEqual(self.test_book.type, "book")
         self.assertEqual(self.test_book.cite_key, "petkir")
@@ -40,7 +40,7 @@ class TestReferenceEntity(unittest.TestCase):
         # Test that required fields are set
         self.assertEqual(self.test_book.author, "Petteri")
         self.assertEqual(self.test_book.editor, "Petteri")
-        self.assertEqual(self.test_book.title, "Petterin kirja vol 2")
+        self.assertEqual(self.test_book.title, "Petterin Kirja vol 2")
         self.assertEqual(self.test_book.publisher, "WSOY")
         self.assertEqual(self.test_book.year, "2004")
 
@@ -50,6 +50,14 @@ class TestReferenceEntity(unittest.TestCase):
         with self.assertRaises(ValueError):
             Article(cite_key="petpet", author="Petteri",
                     title="Petterin Kirja", journal="Petterin Kirjakokoelma")
+    
+    def test_init_book_with_missing_required_arguments(self):
+        """ Test that Book-entity raises ValueError with invalid arguments """
+
+        with self.assertRaises(ValueError):
+            Book(cite_key="petkir", author="Petteri",
+                 editor="Petteri", publisher="WSOY",
+                 year="2004")
 
     def test_article_str_method(self):
         """ Test that Article entitys str method returns correct string """
@@ -69,3 +77,22 @@ class TestReferenceEntity(unittest.TestCase):
                                        "'issn': None, "
                                        "'zblnumber': None, "
                                        "'eprint': None}"))
+
+    def test_book_str_method(self):
+        """ Test that Book entitys str method returns correct string """
+        book_str = str(self.test_book)
+        self.assertEqual(book_str, ("{'_type': 'book', "
+                                       "'_cite_key': 'petkir', "
+                                       "'author': 'Petteri', "
+                                       "'editor': 'Petteri', "
+                                       "'title': 'Petterin Kirja vol 2', "
+                                       "'publisher': WSOY, "
+                                       "'year': '2004', "
+                                       "'number': None, "
+                                       "'volume': None, "
+                                       "'pages': None, "
+                                       "'month': None, "
+                                       "'note': None, "
+                                       "'doi': None, "
+                                       "'issn': None, "
+                                       "'isbn': None}"))
