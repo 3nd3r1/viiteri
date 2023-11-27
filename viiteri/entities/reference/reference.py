49f63ee2af1ba48fe3cd287198dbc20ed32dbbef
3nd3r1 <ender.spam.guru@gmail.com>
2023-11-27 12:51:55 +0200

    Lisätään esimerkki ieee ja bibtex konversio metodeille.

--- viiteri/entities/reference/reference.py
+++ viiteri/entities/reference/reference.py
@@ -12,6 +12,14 @@ class Reference(ABC):
     def __str__(self):
         return str(self.__dict__)
 
+    # @abstractmethod
+    # def format_ieee(self):
+    #    """ Return IEEE formatted reference """
+
+    # @abstractmethod
+    # def format_bibtex(self):
+    #    """ Return BibTeX formatted reference """
+
     @property
     def cite_key(self):
         """ Return cite key """
