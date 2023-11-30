639b76b2efe8747215f7eac53550cc493e367d6a
3nd3r1 <ender.spam.guru@gmail.com>
2023-11-30 14:38:23 +0200

    Templatejen ei tarvitse huolehtia printtauksesta

--- viiteri/routes/list_references.py
+++ viiteri/routes/list_references.py
@@ -1,7 +1,6 @@
 """ viiteri/routes/list_references.py """
 from flask import Blueprint, render_template
 from viiteri.services.reference_service import reference_service
-from viiteri.entities.references import Article, Book, Inproceedings
 
 
 blueprint = Blueprint("list_references", __name__)
@@ -11,14 +10,4 @@ blueprint = Blueprint("list_references", __name__)
 def render_list():
     """ Render listing page """
     references = reference_service.get_all_references()
-    articles, books, inproceedings = [], [], []
-
-    for ref in references:
-        if isinstance(ref, Article):
-            articles.append(ref)
-        elif isinstance(ref, Book):
-            books.append(ref)
-        elif isinstance(ref, Inproceedings):
-            inproceedings.append(ref)
-
-    return render_template("list.html", articles=articles, books=books, inproceedings=inproceedings)
+    return render_template("list.html", references=references)
