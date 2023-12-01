09a662316fc2766af4a38e84c7886b2e7b3ddfe8
ayriainen <lounastarjous@gmail.com>
2023-12-01 14:33:22 +0200

    Ongelmakohta try:n sisään ja kommentoin flashit nyt aluksi pois

--- viiteri/routes/add.py
+++ viiteri/routes/add.py
@@ -16,16 +16,16 @@ def add_reference():
         last_ref_type = session.get("last_ref_type", "article")
         return render_template("add.html", submitted=submitted, last_ref_type=last_ref_type)
     if request.method == "POST":
-        cite_key = request.form["title"][0:3] + \
-            request.form["author"].split(" ")[0][0:3]
         ref_type = request.form.get("ref_type")
         try:
+            cite_key = request.form["title"][0:3] + \
+                request.form["author"].split(" ")[0][0:3]
             reference_service.create_reference(
                         ref_type, cite_key=cite_key, **request.form.to_dict())
-            flash("Reference created successfully!")
+            #flash("Reference created successfully!")
             session["submitted"] = True
             session["last_ref_type"] = ref_type
         except Exception as error:
-            flash(str(error))
+            #flash(str(error))
             session["submitted"] = False
     return redirect("/add")
