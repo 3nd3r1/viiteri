273c6942004d66f9b49beb24e5249d0e888cd8e4
Jouko Mälkönen <jouko.malkonen@helsinki.fi>
2023-12-06 13:09:26 +0200

    linttaus

--- viiteri/entities/references/book.py
+++ viiteri/entities/references/book.py
@@ -1,6 +1,6 @@
 """ viiteri/entities/references/book.py """
 
-# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-instance-attributes, duplicate-code
 from viiteri.entities.references import Reference
 
 
