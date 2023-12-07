944bc8300889ef3b73d4e67128d24a9f64dcd6c0
oskarruo <oskar.ruokolainen@gmail.com>
2023-12-07 23:36:40 +0200

    pylint korjaukset

--- viiteri/services/reference_service.py
+++ viiteri/services/reference_service.py
@@ -5,14 +5,13 @@ from requests import get
 from requests.exceptions import Timeout
 from bibtexparser import loads
 
+from subprocess import run, PIPE
+from bibtexparser import loads
 from viiteri.repositories.reference_repository import reference_repository as default_repository
 from viiteri.entities.references import Reference
 from viiteri.utils.reference_factory import ReferenceFactory
-from viiteri.utils.filter import keyword_filter_references
-
 from subprocess import run, PIPE
 from bibtexparser import loads
-
 
 class ReferenceService:
     """ Service for handling references """
@@ -48,13 +47,14 @@ class ReferenceService:
     def remove_reference(self, ref_id: int):
         """ Removes a reference """
         return self._reference_repository.remove_reference(ref_id)
-    
+
     def get_reference_by_doi(self, doi: str):
+        """ Returns a reference dictionary, takes a doi as input """
         command = "doi2bib " + doi
-        string = run(command, shell=True, stdout=PIPE).stdout.decode('utf-8')
-        dict = loads(string).entries_dict
-        cite_key = next(iter(dict))
-        data_dict = dict[cite_key]
+        bib_string = run(command, shell=True, stdout=PIPE, check=False).stdout.decode('utf-8')
+        bib_dict = loads(bib_string).entries_dict
+        cite_key = next(iter(bib_dict))
+        data_dict = bib_dict[cite_key]
         return data_dict
 
     def get_reference_by_doi(self, doi: str):
