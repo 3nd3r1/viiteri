41c3d6ed223a71d5c3fa2e866f862cab32df04bf
ayriainen <lounastarjous@gmail.com>
2023-12-10 04:48:26 +0200

    Järjestyvät headerit

--- viiteri/routes/list_references.py
+++ viiteri/routes/list_references.py
@@ -1,5 +1,5 @@
 """ viiteri/routes/list_references.py """
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, request
 from sqlalchemy.exc import SQLAlchemyError
 
 from viiteri.services.reference_service import reference_service
@@ -11,9 +11,14 @@ blueprint = Blueprint("list_references", __name__)
 @blueprint.route("/list")
 def render_list():
     """ Render listing page """
-    references = []
+    sort_type = request.args.get('sort')
+    sort_order = request.args.get('order', 'asc')
+
     try:
-        references = reference_service.get_all_references()
+        if sort_type:
+            references = reference_service.get_sorted_references(sort_type, sort_order)
+        else:
+            references = reference_service.get_all_references()
     except SQLAlchemyError:
         flash("Database failed to get all references", "error")
     return render_template("list.html", references=references)
