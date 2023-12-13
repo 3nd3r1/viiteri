90c5d4da54c2d2d82172c2d59c7ed46d7bd419c6
ayriainen <lounastarjous@gmail.com>
2023-12-13 18:35:12 +0200

    Filtteröinnin ja sorttauksen yhteensopivuus

--- viiteri/routes/list_references.py
+++ viiteri/routes/list_references.py
@@ -1,4 +1,3 @@
-#pylint: disable=no-member
 """ viiteri/routes/list_references.py """
 from flask import Blueprint, render_template, flash, request
 from sqlalchemy.exc import SQLAlchemyError
@@ -11,15 +10,17 @@ blueprint = Blueprint("list_references", __name__)
 
 @blueprint.route("/list")
 def render_list():
-    """ Render listing page """
+    search_query = request.args.get('search', '').strip()
     sort_type = request.args.get('sort')
     sort_order = request.args.get('order', 'asc')
-    references = []
+
     try:
-        if sort_type:
-            references = reference_service.get_sorted_references(sort_type, sort_order)
+        if search_query:
+            references = reference_service.get_references(sort_type, sort_order, search_query)
         else:
-            references = reference_service.get_all_references()
+            references = reference_service.get_references(sort_type, sort_order)
     except SQLAlchemyError:
         flash("Database failed to get all references", "error")
-    return render_template("list.html", references=references)
+        references = []
+
+    return render_template("list.html", references=references, search_query=search_query)
